920ce0d312d0dd5196850cc78db3a9dcaf4139d1
Snehal Wadhwani <snehal.wadhwani7@gmail.com>
2023-04-22 15:10:38 -0600

    Heartbeat from broker to guru

--- p3/src/broker.cc
+++ p3/src/broker.cc
@@ -1,17 +1,28 @@
 #include <grpc/grpc.h>
 #include <grpcpp/grpcpp.h>
 #include <string>
+#include <thread>
 
+#include "timer.hh"
 #include "dps.grpc.pb.h"
 
 using grpc::Channel;
 using grpc::Status;
+using grpc::ClientContext;
 using std::string;
 using std::shared_ptr;
 using std::unique_ptr;
+using std::thread;
 using dps::BrokerServer;
 using dps::GuruServer;
+using dps::HeartbeatRequest;
+using dps::HeartbeatResponse;
+using util::Timer;
 
+#define HEARTBEAT_TIMEOUT       1000
+#define HEART   "\xE2\x99\xA5"
+
+// *************************** Class Definitions ******************************
 class BrokerToGuruClient {
   public:
     BrokerToGuruClient(shared_ptr<Channel> guruchannel);
@@ -32,11 +43,65 @@ class BrokerClient {
     unique_ptr<BrokerServer::Stub> stub_;
 };
 
+// *************************** Volatile Variables *****************************
+uint clusterID;
+uint serverID;
+Timer heartbeatTimer(1, HEARTBEAT_TIMEOUT);
+BrokerToGuruClient* bgClient;
+
+// *************************** Functions *************************************
+
+void runHeartbeatTimer() {
+  heartbeatTimer.start(HEARTBEAT_TIMEOUT);
+  while(heartbeatTimer.get_tick() < heartbeatTimer._timeout) ; // spin
+  int ret = bgClient->SendHeartbeat();
+  runHeartbeatTimer();
+}
+
+
+
+
+BrokerToGuruClient::BrokerToGuruClient(shared_ptr<Channel> guruchannel)
+  : gurustub_(GuruServer::NewStub(guruchannel)) {}
+
+int BrokerToGuruClient::SendHeartbeat() {
+  HeartbeatRequest request;
+  HeartbeatResponse response;
+  Status status;
+  ClientContext context;
+
+  request.set_serverid(serverID);
+  request.set_clusterid(clusterID);
+  response.Clear();
+  status = gurustub_->SendHeartbeat(&context, request, &response);
+
+  if(status.ok()) {
+    printf("[SendHeartbeat RPC] %s %s %s Successfully sent heartbeat to Guru %s %s %s\n", HEART, HEART, HEART, HEART, HEART, HEART);
+    return 0;
+  } else {
+    printf("[SendHeartbeat RPC] Failure.\n");
+    return -1;
+  }
+}
+
+
+
 class BrokerGrpcServer final : public BrokerServer::Service {
   public:
     explicit BrokerGrpcServer() {}
 };
 
 int main(int argc, char* argv[]) {
+  if(argc != 4) {
+    printf("Usage: ./broker <serverid> <clusterid> <guruAddr>\n");
+    return 0;
+  }
+  serverID = atoi(argv[1]);
+  clusterID = atoi(argv[2]);
+  bgClient = new BrokerToGuruClient(grpc::CreateChannel(argv[3], grpc::InsecureChannelCredentials()));
+
+  thread heartbeat(runHeartbeatTimer);
+
+  if(heartbeat.joinable()) heartbeat.join();
   return 0;
 }
--- p3/src/guru.cc
+++ p3/src/guru.cc
@@ -1,16 +1,44 @@
 #include <grpc/grpc.h>
 #include <grpcpp/grpcpp.h>
 #include <string>
+#include <vector>
+#include <thread>
 
+#include "timer.hh"
 #include "dps.grpc.pb.h"
 
 using grpc::Channel;
+using grpc::Server;
+using grpc::ServerBuilder;
 using grpc::Status;
+using grpc::ServerContext;
 using std::string;
 using std::shared_ptr;
 using std::unique_ptr;
+using std::vector;
+using std::thread;
 using dps::BrokerServer;
 using dps::GuruServer;
+using dps::HeartbeatRequest;
+using dps::HeartbeatResponse;
+using util::Timer;
+
+#define BROKER_ALIVE_TIMEOUT    5000
+#define HEART   "\xE2\x99\xA5"
+
+class BrokerTimer {
+  public: 
+    uint brokerid;
+    Timer timer;
+
+    BrokerTimer(int bid, int tick, int timeout) 
+      : timer(tick, timeout) {
+      brokerid = bid;
+    }
+};
+
+int CLUSTER_CNT = 1;
+vector<BrokerTimer*> brokerAliveTimers;
 
 class GuruToBrokerClient {
   public:
@@ -24,8 +52,45 @@ class GuruToBrokerClient {
 class GuruGrpcServer final : public GuruServer::Service {
   public:
     explicit GuruGrpcServer() {}
+
+    Status SendHeartbeat(ServerContext *context, const HeartbeatRequest *request, HeartbeatResponse *response) override
+    {
+      bool rpcSuccess = false;
+      int brokerid = request->serverid();
+      int clusterid = request->clusterid();
+
+      printf("Received %s from broker %d in cluster %d\n", HEART, brokerid, clusterid);
+
+      for(BrokerTimer* bt : brokerAliveTimers) {
+        if(bt->brokerid == brokerid) {
+          bt->timer.reset(BROKER_ALIVE_TIMEOUT);
+        } else {
+          if (bt->timer.get_tick() > BROKER_ALIVE_TIMEOUT) {
+            printf("Brokerid: %d down.\n", bt->brokerid);
+            // Start election for topics for brokerid
+          }
+        }
+      }
+      return Status::OK;
+    }
 };
 
+void RunGrpcServer(string server_address) {
+  GuruGrpcServer service;
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server = builder.BuildAndStart();
+  std::cout << "[RunGrpcServer] Server listening on " << server_address << std::endl;
+  server->Wait();
+}
+
 int main(int argc, char* argv[]) {
+  // For testing 
+  for(int j = 0; j<3; j++) {
+    brokerAliveTimers.push_back(new BrokerTimer(j, 1, BROKER_ALIVE_TIMEOUT));
+  }
+
+  RunGrpcServer("0.0.0.0:50050");
   return 0;
 }
